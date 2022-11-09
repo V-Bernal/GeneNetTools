beaca7989fd8c04fc4d4f6b77884605a88916cb1
Venustiano <v.soancatl.aguilar@rug.nl>
2022-11-09 22:28:15 +0100

    ecoli data

--- GeneNetTools/R/c_pcor_shrunk.R
+++ GeneNetTools/R/c_pcor_shrunk.R
@@ -63,41 +63,92 @@ validate_json_file <- function(fileparams) {
 }
 
 
-#' Pcor_Shrunk
+#' ci_pcor_shrunk
 #'
-#' @param lparams
+#' This function computes confidence intervals for the partial correlation with shrinkage.
+#' @param lparams a list of parameters created using the `validate_json_file` function, which receives
+#' a JSON file. This file should contain the following name/value pairs.
 #'
-#' @return plot
+#' "filename": <string, required>
+#'
+#' "variables": <array, strings representing column names>
+#'
+#' "cutoff": <number, required threshold for the p-value of the partial correlation>
+#'
+#' "verbose": <boolean, required to display detailed description on the terminal>
+#'
+#' @return Forest plot of partial correlations in Rplot.pdf
 #' @export
 #'
 # #' @examples
-c_pcor_shrunk <- function(lparams){
+ci_pcor_shrunk <- function(lparams){
   # read file
   dt <- read_data(lparams$filename,lparams$variables)
 
-  result <- GGM.shrunk(x = as.matrix(dt), lambda = lparams$lambda,
+  estimated.pcor.ecoli <- GGM.shrunk(x = as.matrix(dt),
                        verbose = lparams$verbose)
-  print(length(result))
-  GGM <- vec2sm(
-    result
-  )
 
-  GGM[abs(GGM) < lparams$cutoff] <- 0
-  print(GGM)
+  # GGM <- vec2sm(
+  #   result
+  # )
 
+  # p-values
+  # pval.tts.ecoli <- ttest.shrunk(x = result)
+  #
+  # # GGM[abs(GGM) < lparams$cutoff] <- 0
+  # # print(GGM)
+  #
+  # GGM <- vec2sm(
+  #   pval.tts.ecoli
+  # )
+  #
+  # GGM <- abs(GGM) < lparams$cutoff
+  # diag(GGM) <- 0
+  #
+  # g1 <-
+  #   igraph::graph_from_adjacency_matrix(
+  #     abs(GGM),
+  #     mode = c("undirected"),
+  #     diag = FALSE,
+  #     weighted = TRUE
+  #   )
+  #
+  # plot(g1,
+  #      edge.width = 3,
+  #      edge.color = "#fc8d62",
+  #      vertex.color = "#8da0cb", vertex.size = 3,
+  #      vertex.frame.color = "#8da0cb",
+  #      vertex.label.color = "black",
+  #      vertex.label.cex = 1,
+  #      vertex.label.dist = 1,
+  #      vertex.label = colnames(dt),
+  #      layout = igraph::layout
+  # )
 
-  diag(GGM) <- 0
+  #--------------------
+  # Confidence intervals
+  ci.ecoli <- confint.GGM(x = estimated.pcor.ecoli , alpha = 0.05)
+  ci.ecoli
 
-  g1 <-
-    igraph::graph_from_adjacency_matrix(
-      abs(GGM),
-      mode = c("undirected"),
-      diag = FALSE,
-      weighted = TRUE
-    )
+  ci.ecoli <- ci.ecoli[order(ci.ecoli[,1], decreasing = T),]
+  ci.ecoli
 
-  plot(g1 ,
-       layout = igraph::layout_nicely(g1)
-  )
+  # CI forest plot
+  ci.ecoli.df = data.frame(ci.ecoli[ci.ecoli[,2]>0, ])
 
+  # un-comment for the complete list
+  ci.ecoli.df = ci.ecoli.df[1:20,]
+
+  p <- ggplot2::ggplot(data=ci.ecoli.df, ggplot2::aes(y= 1:nrow(ci.ecoli.df),
+                               x=ci.ecoli.df[,1], xmin=ci.ecoli.df[,2],
+                               xmax=ci.ecoli.df[,3])) +
+    ggplot2::geom_point() +
+    ggplot2::geom_errorbarh(height=.4) +
+    ggplot2::labs(title='', x='scaled pcors (95% CI)', y = '') +
+    ggplot2::geom_vline(xintercept=c(0,.1,.3), color='black', linetype='dashed', alpha=.5) +
+    ggplot2::scale_y_continuous(name = "", breaks=1:nrow(ci.ecoli.df), labels=row.names(ci.ecoli.df))+
+    ggplot2::theme(panel.border = ggplot2::element_rect(fill = "transparent", color = "black"),
+                   panel.background = ggplot2::element_rect(fill = "white"))
+
+  print(p)
 }
--- GeneNetTools/tests/testthat/test-pcor_shrunk_api.R
+++ GeneNetTools/tests/testthat/test-pcor_shrunk_api.R
@@ -2,5 +2,5 @@ test_that("shrunk works", {
   js <- "params/shrunk.json"
   lp <- validate_json_file(js)
   validate_parameters(js,"pcor_shrunk_schema.json")
-  c_pcor_shrunk(lp)
+  ci_pcor_shrunk(lp)
 })
